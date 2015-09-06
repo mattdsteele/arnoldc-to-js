1861e066d14e5794e399805b8b4f02d4466e7e9c
Matt Steele <orphum@gmail.com>
2015-09-06 15:37:49 -0500

    Add ES6 build step
    source files live in src, compiled in lib

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,35 @@
 
 var del = require('del');
 var gulp = require('gulp');
+var babel = require('gulp-babel');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
 
 gulp.task('clean', function() {
   del(['test/output']);
+  del(['lib']);
 });
 
-gulp.task('lint', function() {
+gulp.task('lint', ['compile'], function() {
   return gulp.src(['bin/**.js', 'lib/**.js', 'test/**.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('test', ['clean'], function () {
+gulp.task('test', ['clean', 'compile'], function () {
   return gulp.src('test/**/*Test.js', {read: false})
     .pipe(mocha({reporter: 'nyan'}));
 });
 
-gulp.task('default', ['lint', 'test']);
+gulp.task('jison', function() {
+  return gulp.src('src/**/*.jison')
+    .pipe(gulp.dest('lib'));
+});
+
+gulp.task('compile', function() {
+  return gulp.src('src/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('lib'));
+});
+
+gulp.task('default', ['clean', 'jison', 'compile', 'lint', 'test']);
--- src/Transpiler.js
+++ src/Transpiler.js
@@ -1,27 +1,27 @@
-'use strict';
-
 var jison = require('jison');
 var path = require('path');
 var fs = require('fs');
 
 var sourceMap = require('source-map'),
-    SourceNode = sourceMap.SourceNode;
+  SourceNode = sourceMap.SourceNode;
 
-var withSourceMaps = function withSourceMaps(nodes, fileName) {
-  var preamble = new SourceNode(null, null, null, '').add('(function() {\n "use strict";\n');
+var withSourceMaps = function(nodes, fileName) {
+  var preamble = new SourceNode(null, null, null, '')
+    .add('(function() {\n "use strict";\n');
 
-  var codes = nodes.forEach(function (expr) {
+  var codes = nodes.forEach(function(expr) {
     preamble.add(expr.compile(1, fileName));
   });
 
-  var postscript = new SourceNode(null, null, null, '').add('}());');
+  var postscript = new SourceNode(null, null, null, '')
+    .add('}());');
 
   preamble.add(postscript);
 
   return preamble;
 };
 
-var transpile = function transpile(source, fileName) {
+var transpile = function(source, fileName) {
   var bnf = fs.readFileSync(path.join(__dirname, './arnoldc.jison'), 'utf-8');
   var parser = new jison.Parser(bnf);
   parser.yy = require('./ast');
