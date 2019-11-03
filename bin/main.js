20760e1170987b02d3147d59e72672404d10e973
mattdsteele <orphum@gmail.com>
2019-11-03 13:19:03 -0600

    Add publish step

--- bin/main.js
+++ bin/main.js
@@ -3,25 +3,25 @@ var fs = require('fs');
 var Transpiler = require('../lib/Transpiler');
 
 if (process.argv[2]) {
-	var fileName = process.argv[2];
+  var fileName = process.argv[2];
 
-	var ext = fileName.split('.');
+  var ext = fileName.split('.');
 
-	if (ext.length > 0 && ext[ext.length -1] === 'arnoldc') {
+  if (ext.length > 0 && ext[ext.length - 1] === 'arnoldc') {
     var mapping = Transpiler.transpile(fs.readFileSync(fileName, 'utf-8'), fileName);
 
     var jsFile = fileName + '.js',
       sourceMapName = jsFile + '.map';
 
     var output = mapping.toStringWithSourceMap({ file: sourceMapName });
-    
+
     //add source map
     output.code += "\n//# sourceMappingURL=" + sourceMapName.replace(/^.*\//g, '');
     fs.writeFileSync(sourceMapName, output.map);
     fs.writeFileSync(jsFile, output.code);
-	} else {
-		console.log('File must have arnoldc extension');
-	}
+  } else {
+    console.log('File must have arnoldc extension');
+  }
 } else {
-	console.log('You must specify a file');
+  console.log('You must specify a file');
 }
