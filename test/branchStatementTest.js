a353dc9ed3c7177b4e3f8e6c2ad0f060498d10da
Jessica C <jcake2@gmail.com>
2024-02-11 16:55:53 -0600

    Added variable comparison in branchs - killed mutant on line 90

--- test/branchStatementTest.js
+++ test/branchStatementTest.js
@@ -10,4 +10,7 @@ describe('branches', function() {
     it('should function with if, if else, and else statements', function(done) {
       doTest('branches/03_if-elseif-else-statements', done);
     });
+    it('should function with if and else statements with variable expressions', function(done) {
+      doTest('branches/04_if-else-statements-with-variable-expressions', done);
+    });
 });
---
+++ test/fixtures/branches/04_if-else-statements-with-variable-expressions.js
@@ -0,0 +1,12 @@
+(function() {
+    "use strict";
+    (function() {
+      var a = 10;
+      var b = 5;
+      if (a > b) {
+        console.log("a is greater b");
+      } else {
+        console.log("b is greater than or equal to a")
+      }
+    }());
+  }());
