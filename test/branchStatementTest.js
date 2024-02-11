f3715372d578bf57924e9bb3664775e301559ea7
Jessica C <jcake2@gmail.com>
2024-02-10 18:06:46 -0600

    Else statements and coverage gets even better!

--- test/branchStatementTest.js
+++ test/branchStatementTest.js
@@ -4,4 +4,7 @@ describe('branches', function() {
     it('should function with an if statement', function(done) {
       doTest('branches/01_if-statement', done);
     });
+    it('should function with if and else statements', function(done) {
+      doTest('branches/02_if-else-statements', done);
+    });
 });
---
+++ test/fixtures/branches/02_if-else-statements.js
@@ -0,0 +1,10 @@
+(function() {
+    "use strict";
+    (function() {
+      if (false) {
+        console.log("It's false");
+      } else {
+        console.log("It's not false");
+      }
+    }());
+  }());
